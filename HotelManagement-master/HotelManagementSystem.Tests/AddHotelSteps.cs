c5658fb9f2593505965a3bd2f8973fa3da63e529
Paarth Vashisht <pvashisht@tavisca.com>
2018-08-12 15:53:04 +0530

    Updated the Specflow files and the hotel controller file

---
+++ HotelManagement-master/HotelManagementSystem.Tests/AddHotelSteps.cs
@@ -0,0 +1,80 @@
+﻿using FluentAssertions;
+using HotelManagementSystem.Models;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using TechTalk.SpecFlow;
+using TechTalk.SpecFlow.Assist;
+
+namespace HotelManagementSystem.Tests
+{
+    [Binding]
+    public class AddHotelSteps
+    {
+        private Hotel hotel = new Hotel();
+        private Hotel addHotelResponse;
+        private List<Hotel> hotels = new List<Hotel>();
+       // private List<Hotel> hotelsOriginal = new List<Hotel>();
+
+        [Given(@"User provided valid Id '(.*)'  and '(.*)'for hotel")]
+        public void GivenUserProvidedValidIdAndForHotel(int id, string name)
+        {
+            hotel.Id = id;
+            hotel.Name = name;
+        }
+
+        [Given(@"Use has added required details for hotel")]
+        public void GivenUseHasAddedRequiredDetailsForHotel()
+        {
+            SetHotelBasicDetails();
+        }
+
+        [When(@"User calls AddHotel api")]
+        [Given(@"User calls AddHotel api")]
+        public void WhenUserCallsAddHotelApi()
+        {
+            hotels = HotelsApiCaller.AddHotel(hotel);
+        }
+
+        [Then(@"Hotel with name '(.*)' should be present in the response")]
+        public void ThenHotelWithNameShouldBePresentInTheResponse(string name)
+        {
+            hotel = hotels.Find(htl => htl.Name.ToLower().Equals(name.ToLower()));
+            hotel.Should().NotBeNull(string.Format("Hotel with name {0} not found in response",name));
+        }
+
+        [When(@"User provides a valid Id '(.*)' for hotel")]
+        public void GivenUserProvidesAValidIdForHotel(int id)
+        {
+            hotel = HotelsApiCaller.GetHotelsById(id);
+        }
+
+        [Then(@"Hotel with id '(.*)' should be present in the response")]
+        public void ThenHotelWithIdShouldBePresentInTheResponse(int id)
+        {
+            hotel.Id.Should().Equals(id);
+        }
+
+        [Then(@"Hotel with id '(.*)' should be present")]
+        public void ThenHotelWithIdShouldBePresent(string values)
+        {
+            var s = values.Split(',');
+            foreach (string s1 in s )
+            {
+                var check = hotels.Find(ht => ht.Id==int.Parse(s1));
+                check.Should().NotBeNull();
+            }
+        }
+
+
+
+        private void SetHotelBasicDetails()
+        {
+            hotel.ImageURLs = new List<string>() { "image1", "image2" };
+            hotel.LocationCode = "Location";
+            hotel.Rooms = new List<Room>() { new Room() { NoOfRoomsAvailable = 10, RoomType = "delux" } };
+            hotel.Address = "Address1";
+            hotel.Amenities = new List<string>() { "swimming pool", "gymnasium" };
+        }
+    }
+}
---
+++ HotelManagement-master/HotelManagementSystem.Tests/SpecFlowFeature1.feature
@@ -0,0 +1,42 @@
+﻿Feature: Add hotel
+	In order to simulate hotel management system
+	As api consumer
+	I want to be able to add hotel,get hotel details and book hotel
+
+@AddHotel
+Scenario Outline: User adds hotel in database by providing valid inputs
+	Given User provided valid Id '<id>'  and '<name>'for hotel 
+	And Use has added required details for hotel
+	When User calls AddHotel api
+	Then Hotel with name '<name>' should be present in the response
+Examples: 
+| id | name   |
+| 1  | hotel1 |
+| 2  | hotel2 |
+| 3  | hotel3 |
+
+
+@GetHotelById
+Scenario Outline: User Adds a hotel and searches for it.
+	Given User provided valid Id '<id>'  and '<name>'for hotel 
+	And Use has added required details for hotel
+	And User calls AddHotel api
+	When User provides a valid Id '<id>' for hotel 
+	Then Hotel with id '<id>' should be present in the response
+Examples: 
+| id | name   |
+| 4  | hotel4 |
+
+@GetAllHotels
+Scenario: User checks whether the list has been updated or not.
+	Given User provided valid Id '7'  and 'Hyatt'for hotel
+	And Use has added required details for hotel
+	And User calls AddHotel api
+	Given User provided valid Id '8'  and 'Novotel'for hotel 
+	And Use has added required details for hotel
+	When User calls AddHotel api
+	
+	Then Hotel with id '7,8' should be present
+					  
+
+	
